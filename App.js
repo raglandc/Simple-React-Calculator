31ae386461f959562ca00c4f5dc044a2e781d453
raglandc <72812105+raglandc@users.noreply.github.com>
2020-11-12 05:14:01 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+import './App.css';
+import ResultComponent from './components/ResultComponent';
+import KeyPadComponent from './components/KeyPadComponent';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      result: ""
+    }
+  }
+
+  onClick = button => {
+    if(button === "=") {
+      this.calculate()
+    }
+    else if(button === "C"){
+      this.reset()
+    }
+    else if(button === "CE"){
+      this.backspace()
+    }
+    else {
+      this.setState({
+        result: this.state.result + button
+      })
+    }
+  };
+
+calculate = () => {
+  var checkResult = ''
+  if (this.state.result.includes('--')){
+    checkResult = this.state.result.replace('--','+')
+  }
+  else {
+    checkResult = this.state.result
+  }
+  try {
+    this.setState({
+      result: (eval(checkResult) || "") + ""
+    })
+  }
+  catch (e) {
+    this.setState({
+      result: "error"
+    })
+  }
+};
+reset = () => {
+    this.setState({
+      result: ""
+    })
+  };
+backspace = () => {
+  this.setState({
+    result: this.state.result.slice(0, -1)
+  })
+};
+render() {
+  return (
+    <div>
+      <div className="calulator-body">
+        <h1>Simple Calculator</h1>
+        <ResultComponent result={this.state.result}/>
+        <KeyPadComponent result={this.onClick}/>
+      </div>
+    </div>
+  );
+}
+}
+
+
+export default App;
